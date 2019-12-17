d4f6231e628f080b46bf666bc481c52b82ea1a42
Michael Bloch <mchblch@gmail.com>
2019-12-17 22:52:07 +0100

    stored pattern

---
+++ Test/Pattern.h
@@ -0,0 +1,79 @@
+#include "Globals.h"
+
+template<template<typename> class... Ex>
+class Test : public Ex<Test<Ex...>>...
+{
+public:
+	Test()
+	{
+	}
+
+	template<template<typename> class... T>
+	Test(T<Test>&&... arg)
+		: T<Test>(std::move(arg))...
+	{
+	}
+
+	template<template<typename> class... T>
+	Test(const Test<T...>& cast)
+		: T<Test>(static_cast<T<Test>>(*reinterpret_cast<T<Test>*>(static_cast<T<Test<T...>>*>(&cast))))...
+	{
+	}
+
+	template<template<typename> class... T>
+	Test(Test<T...>&& cast)
+		: T<Test>(static_cast<T<Test>&&>(*reinterpret_cast<T<Test>*>(static_cast<T<Test<T...>>*>(&cast))))...
+	{
+	}
+};
+
+template<typename Impl>
+class ETest1
+{
+	Impl* pthis = static_cast<Impl*>(this);
+
+public:
+	ETest1() = default;
+
+	ETest1(int a)
+		: m_int(a)
+	{
+	}
+
+	void print_int()
+	{
+		std::cout << "Integer is: " << m_int << " String is: " << *static_cast<Impl*>(this)->get_string() << '\n';
+	}
+
+private:
+	int m_int = 0;
+};
+
+template<typename Impl>
+class ETest2
+{
+public:
+	ETest2()
+		: m_string(std::make_unique<int>(90))
+	{
+
+	}
+	ETest2(const ETest2&) = delete;
+	ETest2(ETest2&& val)
+		: m_string(std::move(val.m_string))
+	{
+
+	}
+	ETest2(int a)
+		: m_string(std::make_unique<int>(a))
+	{
+	}
+
+	auto& get_string()
+	{
+		return m_string;
+	}
+
+private:
+	std::unique_ptr<int> m_string;
+};
--- Test/main.cpp
+++ Test/main.cpp
@@ -84,102 +84,25 @@ private:
 };
 
 
-template<template<typename> class... Ex>
-class Test : public Ex<Test<Ex...>>...
-{
-public:
-	Test()
-	{
-	}
-
-	template<template<typename> class... T>
-	Test(T<Test>&&... arg)
-		: T<Test>(std::move(arg))...
-	{
-	}
-
-	template<template<typename> class... T>
-	Test(const Test<T...>& cast)
-		: T<Test>(static_cast<T<Test>>(*reinterpret_cast<T<Test>*>(static_cast<T<Test<T...>>*>(&cast))))...
-	{
-	}
-
-	template<template<typename> class... T>
-	Test(Test<T...>&& cast)
-		: T<Test>(static_cast<T<Test>&&>(*reinterpret_cast<T<Test>*>(static_cast<T<Test<T...>>*>(&cast))))...
-	{
-	}
-};
-
-template<typename Impl>
-class ETest1
-{
-public:
-	ETest1() = default;
-
-	ETest1(int a)
-		: m_int(a)
-	{
-	}
-
-	void print_int()
-	{
-		std::cout << "Integer is: " << m_int << " String is: " << *static_cast<Impl*>(this)->get_string() << '\n';
-	}
-
-private:
-	int m_int = 0;
-};
-
-template<typename Impl>
-class ETest2
-{
-public:
-	ETest2()
-		: m_string(std::make_unique<int>(90))
-	{
-
-	}
-	ETest2(const ETest2&) = delete;
-	ETest2(ETest2&& val)
-		: m_string(std::move(val.m_string))
-	{
-
-	}
-	ETest2(int a)
-		: m_string(std::make_unique<int>(a))
-	{
-	}
-
-	auto& get_string()
-	{
-		return m_string;
-	}
-
-private:
-	std::unique_ptr<int> m_string;
-};
-
-
 int main(int argc, char** argv)
 {
 	try
 	{
-		Test<ETest1, ETest2> test1;
-		
-		Test<ETest1> test2(ETest1<Test<ETest1>>(1));
-		test2 = ETest1<Test<ETest1>>(10);
+		//Test<ETest1, ETest2> test1;
+		//
+		//Test<ETest1> test2(ETest1<Test<ETest1>>(1));
+		//test2 = ETest1<Test<ETest1>>(10);
 
-		Test<ETest2> test3(ETest2<Test<ETest2>>(80));
-		
+		//Test<ETest2> test3(ETest2<Test<ETest2>>(80));
+		//
 		//Test<ETest1, ETest2> test3(static_cast<ETest2<Test<ETest1, ETest2>>>(
 		//	*reinterpret_cast<ETest2<Test<ETest1, ETest2>>*>(&static_cast<ETest2<Test<ETest2>>>(test2))));
 
-		Test<ETest1, ETest2> test4(std::move(test3));
-		
-		test4.print_int();
+		//Test<ETest1, ETest2> test4(std::move(test3));
+		//
+		//test4.print_int();
 
-		/*sdl::SDL sdl;
+		sdl::SDL sdl;
 		sdl.initIMG(IMG_INIT_JPG | IMG_INIT_PNG)
 			.initTTF();
 
@@ -189,7 +112,7 @@ int main(int argc, char** argv)
 		sdl::RunLoop<> loop;
 		loop.add_window(&win);
 
-		loop.run(30);*/
+		loop.run(30);
 	}
 	catch (const std::exception & err)
 	{
