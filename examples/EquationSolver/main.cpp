8449b8ef79203cf43ff78b56a18b2d06ba9ddf64
floppyMike <mchblch@gmail.com>
2020-11-08 08:43:30 +0100

    Hard Example

--- examples/EquationSolver/main.cpp
+++ examples/EquationSolver/main.cpp
@@ -15,7 +15,11 @@ auto main() -> int
 			  << mth::solve("2 * 2") << '\n'
 			  << mth::solve("sqrt( 4 ) ") << '\n'
 			  << mth::solve("2 + 90") << '\n'
-			  << mth::solve("2 * 4.5") << '\n';
+			  << mth::solve("2 * 4.5") << '\n'
+			  << mth::solve("sqrt((6.67 * 10^(-11) * 6 * 10^24) / (2 * 10^7)) *"
+							" (sqrt((2 * 4 * 10^7) / (4 * 10^7 + 2 * 10^7)) - 1)")
+			  << '\n'
+			  << mth::solve("2^3") << '\n';
 
 	return 0;
 }
