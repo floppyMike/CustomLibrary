31cb32b0ef2c0221dd398631f716730ffa0affcd
Michaellu <mchblch@protonmail.com>
2021-01-18 13:45:17 +0100

    revert

--- cmake/CustomLibrary-config.cmake
+++
@@ -1,3 +0,0 @@
-foreach(component ${CustomLibrary_FIND_COMPONENTS})
-    include(${CMAKE_CURRENT_LIST_DIR}/${component}-config.cmake)
-endforeach()
--- cmake/core-config.cmake
+++
@@ -1,4 +0,0 @@
-find_package(SDL2)
-if (NOT SDL2_FOUND)
-	set(SDL2_INCLUDE_DIR "")
-endif()
