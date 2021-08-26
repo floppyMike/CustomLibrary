22243edd5ae779f2e2cb67d61fc5b087b7dd0c86
Michaellu <mchblch@protonmail.com>
2021-08-26 07:42:31 +0200

    Excluded the class for Fonts

--- include/CustomLibrary/SDL/Text.h
+++ include/CustomLibrary/SDL/Text.h
@@ -1,5 +1,7 @@
-#if not defined _CTL_SDL2_Text_ and defined _SDL2TTF_ and defined _SDL2IMG_
+#if not defined _CTL_SDL2_Text_
 #define _CTL_SDL2_Text_
+
+#include <SDL_ttf.h>
 
 #include "SDL_init.h"
 
@@ -12,116 +14,18 @@
 
 namespace ctl::sdl
 {
-	// -----------------------------------------------------------------------------
-	// Font
-	// -----------------------------------------------------------------------------
-
-	/**
-	 * @brief Manages a font object
-	 */
-	class Font
+	namespace detail
 	{
-		struct Unique_Deleter
+		struct Font_Destructor
 		{
-			void operator()(TTF_Font *f) { TTF_CloseFont(f); }
+			void operator()(TTF_Font *f)
+			{
+				TTF_CloseFont(f);
+			}
 		};
+	} // namespace detail
 
-	public:
-		Font()		  = default;
-		Font(Font &&) = default;
-		auto operator=(Font &&) -> Font & = default;
-
-		Font(const Font &) noexcept = delete;
-		auto operator=(const Font &) noexcept = delete;
-
-		/**
-		 * @brief Get the managed font ptr
-		 * @return TTF_Font*
-		 */
-		auto font() noexcept
-		{
-			assert(m_ptr && "Font is not loaded.");
-			return m_ptr.get();
-		}
-
-		/**
-		 * @brief Change the managed font
-		 * @param f new font
-		 */
-		auto font(TTF_Font *f) noexcept { m_ptr.reset(f); }
-
-		/**
-		 * @brief Set a font stype
-		 * @param style Style to use https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_22.html
-		 */
-		void style(int style) { TTF_SetFontStyle(font(), style); }
-
-		/**
-		 * @brief Get the font stype used
-		 * @return Font Style
-		 */
-		auto style() { return TTF_GetFontStyle(font()); }
-
-		/**
-		 * @brief Get the hypertheoretical text dimension
-		 *
-		 * @param text Text to measure on
-		 * @return Dimension
-		 */
-		auto hypo_size(const char *text)
-		{
-			mth::Dim<int> temp;
-			TTF_SizeUTF8(font(), text, &temp.w, &temp.h);
-			return temp;
-		}
-
-	private:
-		std::unique_ptr<TTF_Font, Unique_Deleter> m_ptr;
-	};
-
-	template<template<typename> class... Ex>
-	using EFont = typename MixBuilder<Font, Ex...>::type;
-
-	// -----------------------------------------------------------------------------
-	// Text
-	// -----------------------------------------------------------------------------
-
-	/**
-	 * @brief Manages a text
-	 */
-	class Text : public Texture
-	{
-	public:
-		using base = Texture;
-
-		Text() = default;
-
-		using Texture::Texture;
-
-		/**
-		 * @brief Changes the texture with its associated text
-		 *
-		 * @param t Texture ptr
-		 * @param text Text
-		 */
-		auto text(SDL_Texture *t, const char *text)
-		{
-			this->texture(t);
-			m_text = text;
-		}
-
-		/**
-		 * @brief Get the text
-		 * @return const char *
-		 */
-		[[nodiscard]] constexpr auto text() const noexcept -> const auto & { return m_text; }
-
-	private:
-		std::string m_text;
-	};
-
-	template<template<typename> class... Ex>
-	using EText = typename MixBuilder<Text, Ex...>::type;
+	using Font = std::unique_ptr<TTF_Font, detail::Font_Destructor>;
 
 } // namespace ctl::sdl
 
